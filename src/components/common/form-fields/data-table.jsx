89e6d5da467f68d1b96d80a04a738e1878f0a0ed
Mustafa <yazilimcimiyim@gmail.com>
2024-01-31 21:43:05 +0300

    31.01.2024

--- src/components/common/form-fields/data-table.jsx
+++ src/components/common/form-fields/data-table.jsx
@@ -1,42 +1,54 @@
 import React from "react";
 
-export const Column = ({ title }) => {
-	return <th scope="col">{title}</th>;
+export const Column = ({ title, dataSource, dataKey, children }) => {
+  return <th scope="col">{title}</th>;
+};
+
+export const Row = ({ children }) => {
+  return <tr className="">{children}</tr>;
+};
+
+export const Cell = ({ data }) => {
+  return <td>{data}</td>;
+};
+
+export const NoRecordFound = ({ colLength }) => {
+  return (
+    <tr>
+      <td>No records found</td>
+    </tr>
+  );
 };
 
 const DataTable = ({ title, children }) => {
-	// children korumali bir prop oldugu icin uzerinde degisiklik yapmaya izin vermez
-	// degisiklik yapabilmek icin kopyasini olusturduk
-	const columns = [...children];
+  // children korumali bir prop oldugu icin uzerinde degisiklik yapmaya izin vermez
+  // degisiklik yapabilmek icin kopyasini olusturduk
 
-	return (
-		<div className="card">
-			<div className="card-body">
-				<h3 className="card-title">{title}</h3>
+  if (!dataSource) throw new Error("dataSource attribute is required");
+  if (!Array.isArray(dataSource))
+    throw new Error("dataSource value must be an array");
+  if (!dataKey) throw new Error("dataKey attribute is required");
 
-				<table className="table table-striped ">
-					<thead>
-						<tr>
-							{columns.map((item) => (
-								<Column
-									key={item.props.title}
-									{...item.props}
-								/>
-							))}
-						</tr>
-					</thead>
-					<tbody>
-						<tr className="">
-							<td scope="row">R1C1</td>
-							<td>R1C2</td>
-							<td>R1C3</td>
-						</tr>
-						
-					</tbody>
-				</table>
-			</div>
-		</div>
-	);
+  const columns = [...children];
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h3 className="card-title">{title}</h3>
+
+        <table className="table table-striped ">
+          <thead>
+            <tr>
+              {columns.map((item) => (
+                <Column key={item.props.title} {...item.props} />
+              ))}
+            </tr>
+          </thead>
+          <tbody>{dataSource.length <= 0 ? <NoRecordFound /> : null}</tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default DataTable;
--- src/components/dashboard/home/admin/admin-list.jsx
+++ src/components/dashboard/home/admin/admin-list.jsx
@@ -1,14 +1,22 @@
-import DataTable from "@/components/common/form-fields/data-table";
+import DataTable, { Column } from "@/components/common/form-fields/data-table";
 import Link from "next/link";
 import React from "react";
 
 const AdminList = ({ data }) => {
+
+	const{content}=data;
+
 	return (
 		<div className="container">
 			<Link href="/dashboard/admin/new" className="btn btn-primary mb-3">
 				New
 			</Link>
-            <DataTable title="Admin List"/>
+            <DataTable title="Admin List" dataSource={content} dataKey="id">
+				<Column title="First Name"/>
+				<Column title="LAst Name"/>
+				<Column title="Username"/>
+
+			</DataTable>
 		</div>
 	);
 };
